10c895933146bfc3a97d6eb281b790e9d890d3e5
Mael <legrand.mael2002@gmail.com>
2020-11-02 13:48:48 +0100

    Correction bug majeur
    Refonte de l'HTML et correction de quelques bug

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let actualPage = "home";
 
 function displayPage(page){
+    
     console.log(actualPage); 
+    
     document.getElementById(actualPage).style.display = "none";
+    
     console.log('Redirection vers la page : '+page);
+    
     let pageToDisplay = document.getElementById(page);
+    
     pageToDisplay.style.display = "flex";
+    
     actualPage = page;
 }
