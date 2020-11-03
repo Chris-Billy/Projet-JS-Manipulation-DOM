d03674c930a1e82e75a297c72f74dc188e38b7db
Mael <legrand.mael2002@gmail.com>
2020-11-03 12:52:10 +0100

    Ajout de l'heure auto-refresh

--- script.js
+++ script.js
@@ -2,25 +2,19 @@ let actualPage = "home";
 let title = document.getElementById('title');
 let background = document.getElementById('container');
 let fabio = document.getElementById('yamfabio');
+let timed = document.getElementById('date');
+let timed2 = document.getElementById('datep');
 
 function displayPage(page){
-    
-    console.log(actualPage); 
-    
+    console.log(actualPage);
     document.getElementById(actualPage).style.display = "none";
-    
     console.log('Redirection vers la page : '+page);
-    
     let pageToDisplay = document.getElementById(page);
-    
     pageToDisplay.style.display = "flex";
     background.className = '';
     background.classList.add(page + "-bg")
-    
-    
     actualPage = page;
 }
-
 
 function replaceText(text){
     title.innerHTML = text;
@@ -30,9 +24,26 @@ function defaultText(){
     title.innerHTML = "Découvrez les pilotes de ces écuries!";
 }
 
-function displayTxt(fText, fText2){
+function displayElem(fText, fText2){
     let hidTxt = document.getElementById(fText);
     let showTxt = document.getElementById(fText2);
     hidTxt.style.display = 'none';
     showTxt.style.display = 'flex';
 }
+
+function dispDate(){
+    let currentTime = new Date();
+    let hh = currentTime.getHours();
+    let m2 = currentTime.getMinutes();
+    let ss = currentTime.getSeconds();
+    let dd = currentTime.getDay();
+    let mm = currentTime.getMonth() + 1;
+    let yyyy = currentTime.getFullYear();
+    timed.innerHTML ='Nous sommes le : ' +dd+'/'+mm+'/'+yyyy ;
+    timed2.innerHTML = 'Il est actuellement :' + hh + 'h' + m2 + 'm' + ss + 's';
+} 
+setInterval(dispDate, 1000);
+
+timed.innerHTML ='Nous sommes le ' + dd +'/'+ mm +'/'+ yyyy ;
+
+timed2.innerHTML = 'Il est actuellement : ' + hh + 'h' + m2 + 'm' + ss + 's';
